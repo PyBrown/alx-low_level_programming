68a7a846d7d800acce725c964b0c4eb1a80efd68
PyBrown <osinaikeanuola@gmail.com>
2022-10-14 22:10:35 -0700

    Shortcut

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	printf("Size of a char: %s byte(s)\n", sizeof(char));
-	printf("Size of a int: %s byte(s)\n", sizeof(int));
+	printf("Size of an int: %s byte(s)\n", sizeof(int));
 	printf("Size of a long int: %s byte(s)\n", sizeof(long));
-	printf("size of a long long int: %s byte(s)\n", sizeof(long long));
+	printf("Size of a long long int: %s byte(s)\n", sizeof(long long));
 	printf("Size of a float: %s byte(s)\n", sizeof(float));
 	return (0);
 }
