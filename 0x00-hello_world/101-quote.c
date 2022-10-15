b067c3cc046b777eaa325e1ffa163d5990698aa8
PyBrown <osinaikeanuola@gmail.com>
2022-10-15 08:07:20 -0700

    Shortcut

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - entrey point
+ *
+ * Description: print text to standard error
+ * followed by new line
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
+	return (1);
+}
