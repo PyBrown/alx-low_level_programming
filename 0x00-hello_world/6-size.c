7f012b36de23dde2092a5eccea5ecd1912d346ec
PyBrown <osinaikeanuola@gmail.com>
2022-10-14 21:52:43 -0700

    Shortcut

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print size of data types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %s byte(s)\n", sizeof(char));
+	printf("Size of a char: %s byte(s)\n", sizeof(int));
+	printf("Size of a char: %s byte(s)\n", sizeof(long));
+	printf("size of a char: %s byte(s)\n", sizeof(long long));
+	printf("Size of a char: %s byte(s)\n", sizeof(float));
+	return(0);
+}
