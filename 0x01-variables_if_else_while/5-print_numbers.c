b6dfbf224b87ed36ea09418227f226f2bee755cc
PyBrown <osinaikeanuola@gmail.com>
2022-10-16 15:58:21 -0700

    Shortcut

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers
+ * of base 10 starting from 0,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
