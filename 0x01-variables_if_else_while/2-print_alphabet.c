e43f16f79cc7cba819e5deae840f05778281e3a4
PyBrown <osinaikeanuola@gmail.com>
2022-10-16 14:56:40 -0700

    Shortcut

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char aplha = "a";
+
+	while (alpha <= "z")
+		putchar(alpha);
+		++alpha;
+	putchar("\n");
+	return (0);
+}
