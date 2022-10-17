b466a7b965c91606dde25ed6f5fd154c6e21b0d1
PyBrown <osinaikeanuola@gmail.com>
2022-10-16 19:50:51 -0700

    Shortcut

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers
+ * of base 10 starting from 0,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num1 = 0; num1 <= 9; num1++)
+		
+	{
+		putchar((num % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+}
