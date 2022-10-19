1184b4dca3dd5e1a396c7982dff7104c506f05a0
PyBrown <osinaikeanuola@gmail.com>
2022-10-19 11:34:30 -0700

    Shortcut

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @x: number being added
+ * @y: number being added
+ *
+ * Description: adds two integers and returns the result
+ *
+ * Return: 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
