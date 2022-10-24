0b4074870ca8f4a8223d242527f5b22f4984aeb3
PyBrown <osinaikeanuola@gmail.com>
2022-10-24 11:52:16 -0700

    C - Pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@
 
 void print_rev(char *s)
 {
-	int index;
+        int index;
 
-	/*finds the length of string without null character*/
-	for (index = 0; s[index] != '\0'; ++index)
-		;
+        /*finds the length of string without null character*/
+        for (index = 0; s[index] != '\0'; ++index)
+                ;
 
-	/*print char from the last index as you decrement*/
-	for (--index; index >= 0; --index)
-		_putchar(s[index]);
-	_putchar('\n');
+        /*print char from the last index as you decrement*/
+        for (--index; index >= 0; --index)
+                _putchar(s[index]);
+        _putchar('\n');
 }
